df608123ac0ca255edd51dc3efa52e2a438fa5e0
Cody Han <codyhan94@gmail.com>
2016-03-19 14:28:58 -0700

    Complete lab 1.

--- lab1/lab1.cc
+++ lab1/lab1.cc
@@ -5,15 +5,21 @@
 
 #define NUM_POINTS 3
 
+/*! Computes the area of the triangle specified by points a, b, and c using
+    Heron's formula. */
 double computeArea(Point &a, Point &b, Point &c)
 {
     double s, ab, bc, ac, area;
 
+    /* Compute the three side lengths in the triangle. */
     ab = a.distanceTo(b);
     bc = b.distanceTo(c);
     ac = a.distanceTo(c);
 
+    /* Compute the semiperimeter. */
     s = (ab + bc + ac) / 2.0;
+
+    /* Compute the actual area. */
     area = sqrt(s * (s - ab) * (s - bc) * (s - ac));
 
     return area;
@@ -25,25 +31,30 @@ int main(int argc, char const *argv[])
     int i;
     double x, y, z, area;
 
+    /* Allocate a buffer on the stack for the number of points we want. */
     Point buf[NUM_POINTS];
 
     for (i = 0; i < NUM_POINTS; i++)
     {
-        cout << "Enter 3 coordinates separated by whitespace for point " << i << ": ";
+        cout << "Enter 3 coordinates separated by whitespace for point " <<
+            i << ": " << endl;
         cin >> x >> y >> z;
         buf[i].setX(x);
         buf[i].setY(y);
         buf[i].setZ(z);
     }
 
+    /* Compute the area of the triangle determined by the 3 points. */
     area = computeArea(buf[0], buf[1], buf[2]);
 
+    /* Echo the points back to the user. */
     for (i = 0; i < NUM_POINTS; i++)
     {
-        cout << buf[i] << endl;
+        cout << "Point " << i << ":\t" << buf[i] << endl;
     }
 
-    cout << "Area:\t" << area << endl;
+    /* Print the computed area. */
+    cout << "Area is:\t" << area << endl;
 
     return 0;
 }
